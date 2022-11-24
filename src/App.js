7399948f235b64f9d908ca0dff1260867af85041
oscar anillo <oscarani14@gmail.com>
2022-11-24 17:26:30 -0500

    add build, numbers, text components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { HeaderComponent } from "./Components/header-component";
+import { NumbersComponent } from "./Components/numbers-component";
+import { TextComponent } from "./Components/text-component";
+import { BuildCommunity } from "./Components/builld-community";
 
 import "./App.css";
 
@@ -6,6 +9,9 @@ function App() {
   return (
     <div className="App">
       <HeaderComponent />
+      <NumbersComponent />
+      <TextComponent />
+      <BuildCommunity />
     </div>
   );
 }
